a5c567e257463a1bd3f5dc56ef38813314b76d41
ballinyouup <bryanhuici@gmail.com>
2026-06-04 11:11:00 -0400

    Updated ip and cors

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,5 +1,14 @@
 
 <?php
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: POST, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type");
+
+	if ($_SERVER["REQUEST_METHOD"] === "OPTIONS")
+	{
+		exit();
+	}
+
 	$inData = getRequestInfo();
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,4 +1,13 @@
 <?php
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: POST, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type");
+
+	if ($_SERVER["REQUEST_METHOD"] === "OPTIONS")
+	{
+		exit();
+	}
+
 	$inData = getRequestInfo();
 
 	$contactId = (int)($inData["id"] ?? 0);
